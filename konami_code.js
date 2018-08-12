f56000cb7c4b7b5dfc83f674b666e6f3bff0d9d1
Casey Zimmer <czimmer@ramapo.edu>
2018-08-12 22:21:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -19,7 +19,7 @@ function init()
   element.innerHTML = ("Marker");
   document.body.appendChild(element);
   element.addEventListener('keydown',konamiCodeCheck)
-  element.addEventListener('click',test(e){alert("hmm");})
+  element.addEventListener('click',test(e){alert("hmm")})
 }
 
 function konamiCodeCheck(e)
